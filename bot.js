2d0b0f12b6f442f121b44908cdc3ccfc1f22b3e4
Amgelo563 <luisangeloarce314@gmail.com>
2021-12-11 23:35:01 -0500

    :recycle: Fix refactor

--- bot.js
+++ bot.js
@@ -5,7 +5,13 @@ const config = require('./config.json');
 
 class Bot {
     constructor() {
-        this.#client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+        this.#client = new Client({
+            intents: [Intents.FLAGS.GUILDS],
+            presence: {
+                status: config.defPresence.status,
+                activities: [config.defPresence.activity]
+            }
+        })
     }
 
     #client;
@@ -16,21 +22,52 @@ class Bot {
         return this.instance;
     }
 
-    async renameServer(client) {
-        const { channel_id, server, format, offline } = config;
-        const channel = await client.channels.fetch(channel_id);
+    async getPlayers() {
+        const response = await (await fetch(`https://api.minetools.eu/ping/${config.server.ip}/${config.server.port}`)).json();
+        if (response.error) return 'offline';
+        return response.players.online;
+    }
 
-        const response = await (await fetch(`https://api.minetools.eu/ping/${server.ip}/${server.port}`)).json();
-        if (response.error) return channel.setName(offline);
+    async updatePresence(client, getPlayers) {
+        const count = await getPlayers();
+        if (count === 'offline') return client.user.setActivity(config.updates.presence.offline);
+        return client.user.setActivity(config.updates.presence.format.replace('%p%', count), {type: config.updates.presence.type});
+    }
 
-        await channel.setName(format.replace('%p%', response.players.online));
+    async updateChannelName(client, getPlayers) {
+        const count = await getPlayers();
+        const channel = await client.channels.fetch(config.updates.channel_name.channel_id);
+        return channel.setName(config.updates.channel_name.format.replace('%p%', count));
+    }
+
+    async updateChannelDesc(client, getPlayers) {
+        const count = await getPlayers();
+        const channel = await client.channels.fetch(config.updates.channel_description.channel_id);
+        return channel.setTopic(config.updates.channel_name.format.replace('%p%', count));
     }
 
     async start() {
         await this.#client.login(config.token);
-        console.log(`Started. Listening to ${config.server.ip}:${config.server.port}`)
-        await this.renameServer(this.#client);
-        setInterval(this.renameServer, config.timeout, this.#client);
+        console.log(`Started. Listening to ${config.server.ip}:${config.server.port}`);
+        
+        if (config.updates.presence.enabled) {
+            await this.updatePresence(this.#client, this.getPlayers);
+            setInterval(this.updatePresence, config.updates.presence.timeout * 1000, this.#client, this.getPlayers)
+        };
+
+        if (config.updates.channel_name.enabled) {
+            await this.updateChannelName(this.#client, this.getPlayers);
+            setInterval(this.updateChannelName, config.updates.channel_name.timeout * 1000, this.#client, this.getPlayers)
+        };
+
+        if (config.updates.channel_description.enabled) {
+            const channel = await this.#client.channels.fetch(config.updates.channel_description.channel_id);
+            if (!channel.isText()) return console.warn(`| The specified channel in channel_description does not have a topic feature.
+| As such, we will not try to update its description. Please change it to another channel or disable this on the config.`);
+            await this.updateChannelDesc(this.#client, this.getPlayers);
+            setInterval(this.updateChannelDesc, config.updates.channel_description.timeout * 1000, this.#client, this.getPlayers)
+        };
+
     }
 }
 
